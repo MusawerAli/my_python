a9305c89829389d0dba50cd24788d3aca43c8c6f
MusawerAli <pakjalihouse@gmail.com>
2019-09-20 21:26:41 +0500

    initial load

---
+++ movie1/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    actor = models.CharField(max_length=30)
+    actor_movie = models.CharField(max_length=50)
+    genre = models.CharField(max_length=20)
+    movie_logo = models.CharField(max_length=30)
+
+  
+class Song(models.Model):
+    movie = models.ForeignKey(Movie, on_delete =models.CASCADE)
+    file_type = models.CharField(max_length=50)
+    song_name = models.CharField(max_length=100)
+    
